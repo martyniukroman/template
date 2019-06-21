89a2724edf650d7456db59482fbbd994f33826d6
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-06-21 17:58:11 +0300

    api

---
+++ template/template.api/Models/Phone.cs
@@ -0,0 +1,9 @@
+namespace template.api.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
