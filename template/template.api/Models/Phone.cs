a1bc852aa2e6bdcf6dc77f4b805e81d5344ba436
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-06-24 14:47:53 +0300

    removed trash

--- template/template.api/Models/Phone.cs
+++
@@ -1,9 +0,0 @@
-namespace template.api.Models
-{
-    public class Phone
-    {
-        public int Id { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
-    }
-}
