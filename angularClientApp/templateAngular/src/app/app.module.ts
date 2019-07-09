6b1b7f240e30813e9d3f2702b269e35f589dea48
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-07-09 17:18:55 +0300

    fixed routing now host/ is home and also host/home is home, but host/dashboard invalid and host/other/dashboard valid

--- angularClientApp/templateAngular/src/app/app.module.ts
+++ angularClientApp/templateAngular/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {LOCALE_ID, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 import {HomeModule} from './modules/home/home.module';
+import {OtherModule} from "./modules/other/other.module";
+import {Page404Component} from "./modules/other/page404/page404.component";
 
 @NgModule({
   declarations: [
@@ -12,12 +14,15 @@ import {HomeModule} from './modules/home/home.module';
   imports: [
     BrowserModule,
     HomeModule,
+    OtherModule,
     RouterModule.forRoot([
       {path: 'home', loadChildren: './modules/home/home.module#HomeModule'},
-      // {path: 'home', component: HomePageComponent}
+      {path: '', loadChildren: './modules/other/other.module#OtherModule'},
+      {path: '**', component: Page404Component},
     ]),
   ],
   providers: [{provide: LOCALE_ID, useValue: 'ru'}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
