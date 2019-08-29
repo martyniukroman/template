5d0e52f731bfe76f1061b12b7f8371da32372146
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-08-29 11:55:39 +0300

    minor

--- angularClientApp/templateAngular/src/app/modules/home/page404/page404.component.ts
+++ angularClientApp/templateAngular/src/app/modules/home/page404/page404.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {getElement} from "devextreme-angular";
 
 @Component({
   selector: 'app-page404',
